e109b38a89d6e79a8c82a232342849e8ae15992c
venkata1git <93291311+venkata1git@users.noreply.github.com>
2025-12-22 17:24:16 -0500

    Attribute Dir_ngclass

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,20 @@
 import { Component, signal } from '@angular/core';
- // 1. Import CommonModule
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
-interface empInterface{
-  id:number;
-  name:string;
-  role:string;
-}
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule], // 2. Add CommonModule here
+  imports: [RouterOutlet, FormsModule, CommonModule], // 2. Add CommonModule here
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  employees = signal<empInterface[]>([
-    // {id:1, name:'John Doe', role:'Developer'},
-    // {id:2, name:'Jane Smith', role:'Designer'},
-    // {id:3, name:'Sam Brown', role:'Manager'}
-  ]);
+  //updateStyles: any = 'updateStyles';
+  hasText: boolean = false;
+  textInput(e: any) {
+    this.hasText = e.target.value !== '';
+  }
 }
