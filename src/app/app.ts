f04412b7a5f6170d0d2cdb9a6112f59a561d6161
venkata1git <93291311+venkata1git@users.noreply.github.com>
2025-12-22 15:53:31 -0500

    Structural Dir_Ngfor

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,9 @@ import { CommonModule } from '@angular/common'; // 1. Import CommonModule
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
-// Define the missing interface here
-interface empInterface {
+export interface FullStackDev {
   id: number;
-  name: string;
-  position: string;
+  name: string; 
 }
 @Component({
   selector: 'app-root',
@@ -17,15 +15,17 @@ interface empInterface {
   styleUrl: './app.css'
 })
 export class App {
-  showEmployees: boolean = false;
-  employees = signal<empInterface[]>([
-    { id: 1, name: 'John Doe', position: 'Admin' },
-    { id: 2, name: 'Jane Smith', position: 'HR' },
-    { id: 3, name: 'Sam Johnson', position: 'Marketing' },
-    { id: 4, name: 'Samuel Joh', position: 'Developer' },
-    { id: 5, name: 'Samanta sony', position: 'Marketing' },
-  ]);
-  toggleEmployees() {
-    this.showEmployees = !this.showEmployees;
-  }
+  fullStackDev = [{
+    id: 1,
+    name: 'Angluar'
+  }, {
+    id: 2,
+    name: 'React'
+  },{
+    id: 3,
+    name: 'Vue'
+  },{
+    id: 4,
+    name: 'Svelte'
+  }]
 }
