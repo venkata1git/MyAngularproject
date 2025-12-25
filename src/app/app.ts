d0054b68b2d996f056cb1552b9e003197733cf06
venkata1git <93291311+venkata1git@users.noreply.github.com>
2025-12-25 18:49:13 -0500

    Component creation hook-ngOnChange()

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Test } from './test/test';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule], // 2. Add CommonModule here
+  imports: [RouterOutlet, FormsModule, CommonModule, Test], // 2. Add CommonModule here
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class  App implements OnInit {  
-
-    ngOnInit(): void {
-        console.log('calling from ngOnInit() hook ...');
-    } 
-
-// export class  App {  
-    // constructor(){
-    //     console.log('App Component Constructor Called...');
-    // } 
-} 
+export class  App {  
+     inputValue: string = 'new value';
+     
+    //  constructor(){
+    //     console.log('Calling from constructor - ', this.inputValue);
+        
+    //  }
+ } 
--- src/app/test/test.ts
+++ src/app/test/test.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-test',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './test.html',
-  styleUrl: './test.css',
+  styleUrl: './test.css'
 })
-export class Test {
+export class Test implements OnChanges {
 
-}
+  @Input() inputValue: string = '';
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Calling from the ngOnChanges hook...');
+    console.log(changes);
+  }
+  
+  }  
