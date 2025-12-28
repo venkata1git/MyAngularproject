b9485528d20f9e2d7aac65a531bbe991f6825b3c
venkata1git <93291311+venkata1git@users.noreply.github.com>
2025-12-28 15:42:40 -0500

    Task-color_ picker @hostbinding and @hostlistener

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit, 
+   HostBinding, HostListener } 
+from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -11,18 +13,20 @@ import { Child } from './child/child';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class  App implements AfterViewInit {  
-     @ViewChild(Child) childComponent?: Child;
-     @ViewChild('btnIncr') btnRef?: ElementRef<HTMLButtonElement>;
+export class App {
+  @ViewChild('colorInput') colorInput!: ElementRef;
+  
+  @HostBinding('style.backgroundColor') selectedColor: string = 'white';
 
-
-     incrChildCounter() {
-      console.log(this.childComponent);
-      this.childComponent?.incrCounter();
-     }
-    ngAfterViewInit(): void{
-      if(this.btnRef?.nativeElement){
-        this.btnRef.nativeElement.innerHTML='Counter ++';
-      }
-    } 
- } 
+  // FIX 1: Change this to just ['$event']
+  // We stop trying to get .value here to avoid the error
+  @HostListener('input', ['$event']) 
+  onColorChange(event: any) { 
+    
+    // FIX 2: We get the value here using 'any' to bypass strict checks
+    const color = event.target.value;
+    
+    this.selectedColor = color;
+    console.log("Color changed to:", color);
+  }
+}
