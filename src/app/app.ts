94949969f4ae97407408e893e8ecd26dec41c366
venkata1git <93291311+venkata1git@users.noreply.github.com>
2025-12-28 20:02:46 -0500

    pipes intro

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit,
    HostBinding, HostListener } 
 from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Child } from './child/child';
 
@@ -14,19 +14,6 @@ import { Child } from './child/child';
   styleUrl: './app.css'
 })
 export class App {
-  @ViewChild('colorInput') colorInput!: ElementRef;
-  
-  @HostBinding('style.backgroundColor') selectedColor: string = 'white';
-
-  // FIX 1: Change this to just ['$event']
-  // We stop trying to get .value here to avoid the error
-  @HostListener('input', ['$event']) 
-  onColorChange(event: any) { 
-    
-    // FIX 2: We get the value here using 'any' to bypass strict checks
-    const color = event.target.value;
-    
-    this.selectedColor = color;
-    console.log("Color changed to:", color);
-  }
+  title: string ='Example of pipe expression';
+  showDt: Date = new Date();
 }
